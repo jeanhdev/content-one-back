de69f2b7fc91fed7c1bbc4a9cac1d132e9719165
Jean Hirtz <jeanhdev@gmail.com>
2021-05-23 17:52:09 +0200

    set lax

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,9 @@ createConnection()
         name: "qid",
         store: new RedisStore({ client: redis, disableTouch: true }),
         cookie: {
-          path: "/",
           maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
           httpOnly: true,
-          sameSite: "none", // csrf
+          sameSite: "lax", // csrf
           secure: __prod__
         },
         proxy: true,
